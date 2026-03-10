ea477d61d9c977dc0a82395391b907357bff5372
vmlinuz719 <vmlinuz719@gmail.com>
2026-03-10 10:19:16 -0400

    think thank thunk

--- not_vibe_code/asm2.c
+++ not_vibe_code/asm2.c
@@ -5,15 +5,6 @@
 
 #define MAX_LABEL_LEN 12
 
-enum event_type {
-    SYMBOL,
-    LABEL_DEF,
-    LIST_ITEM,
-    LIST_END,
-    ENDFILE,
-    ERROR
-};
-
 typedef struct {
     char label[MAX_LABEL_LEN + 1];
     uint64_t value;
@@ -24,7 +15,17 @@ typedef struct {
     int num_labels, max_labels;
 } label_tab_t;
 
+int get_label(label_tab_t *ltab, char *label) {
+    for (int i = 0; i < ltab->num_labels; i++) {
+        if (!strcmp(label, ltab->labels[i].label)) return i;
+    }
+    
+    return -1;
+}
+
 int insert_label_def(label_tab_t *ltab, char *label, uint64_t value) {
+    if (get_label(ltab, label) != -1) return -1;
+    
     if (ltab->num_labels == ltab->max_labels) return -1;
     
     strncpy(ltab->labels[ltab->num_labels].label, label, MAX_LABEL_LEN);
@@ -102,14 +103,6 @@ int remove_thunk(thunk_tab_t *ttab, int index) {
     return 0;
 }
 
-int get_label(label_tab_t *ltab, char *label) {
-    for (int i = 0; i < ltab->num_labels; i++) {
-        if (!strcmp(label, ltab->labels[i].label)) return i;
-    }
-    
-    return -1;
-}
-
 int register_label_do_thunks(
     label_tab_t *ltab, char *label, uint64_t value,
     thunk_tab_t *ttab, uint64_t *work_area
@@ -130,9 +123,10 @@ int register_label_do_thunks(
         v <<= thunk->left_shift;
         work_area[thunk->address] |= v;
         thunks_done++;
+        printf("\n\tThunked [%lu] = %lu",
+            thunk->address, work_area[thunk->address]);
         remove_thunk(ttab, current_thunk);
     }
-    
     return thunks_done;
 }
 
@@ -143,20 +137,38 @@ typedef struct {
     FILE *file;
     
     uint64_t pc;
+    
+    label_tab_t *ltab;
+    thunk_tab_t *ttab;
+    uint64_t *work_area;
 } assembler_ctx_t;
 
-assembler_ctx_t *new_assembler(char *fname) {
+int assembler_get_label(assembler_ctx_t *ctx, char *label) {
+    return get_label(ctx->ltab, label);
+}
+
+assembler_ctx_t *new_assembler(
+    char *fname, int max_labels, int max_thunks, uint64_t *work_area
+) {
     FILE *file = fopen(fname, "r");
     if (file == NULL) return NULL;
     
     assembler_ctx_t *result = calloc(1, sizeof(assembler_ctx_t));
     result->file = file;
     
+    result->ltab = new_label_tab(max_labels);
+    result->ttab = new_thunk_tab(max_thunks);
+    result->work_area = work_area;
+    
     return result;
 }
 
 void delete_assembler(assembler_ctx_t *ctx) {
     fclose(ctx->file);
+    
+    delete_label_tab(ctx->ltab);
+    delete_thunk_tab(ctx->ttab);
+    
     free(ctx);
 }
 
@@ -185,9 +197,18 @@ int read_symbol(assembler_ctx_t *ctx) {
     return 0;
 }
 
+enum event_type {
+    ERROR,
+    SYMBOL,
+    LABEL_DEF,
+    LIST_ITEM,
+    LIST_END,
+    FILE_END
+};
+
 enum event_type get_symbol_type(assembler_ctx_t *ctx) {
     enum event_type result =  ctx->error          ? ERROR
-                            : ctx->eof            ? ENDFILE
+                            : ctx->eof            ? FILE_END
                             : ctx->is_label_def   ? LABEL_DEF
                             : ctx->has_comma      ? LIST_ITEM
                             : ctx->is_end_of_list ? LIST_END
@@ -197,19 +218,104 @@ enum event_type get_symbol_type(assembler_ctx_t *ctx) {
     return result;
 }
 
+int assembler_register_label(
+    assembler_ctx_t *ctx,
+    char *label,
+    uint64_t value
+) {
+    return register_label_do_thunks(
+        ctx->ltab, label, value,
+        ctx->ttab, ctx->work_area
+    );
+}
+
+int assembler_insert_thunk(
+    assembler_ctx_t *ctx,
+    char *label,
+    uint64_t address,
+    int is_relative,
+    int width,
+    int left_shift
+) {
+    return insert_thunk(
+        ctx->ttab,
+        label,
+        address,
+        is_relative,
+        width,
+        left_shift
+    );
+}
+
 int main(int argc, char *argv[]) {
-    assembler_ctx_t *assembler = new_assembler(argv[1]);
+    uint64_t work_area[8192];
+    assembler_ctx_t *assembler = new_assembler(argv[1], 128, 128, work_area);
     
     while (!read_symbol(assembler)) {
         printf("%-16s is ", assembler->buf);
         switch (get_symbol_type(assembler)) {
             case ERROR:     printf("ERROR"); break;
-            case ENDFILE:   printf("ENDFILE"); break;
-            case LABEL_DEF: printf("LABEL_DEF"); break;
+            case FILE_END:  printf("FILE_END"); break;
+            
+            case LABEL_DEF: {
+                printf("LABEL_DEF@%lu", assembler->pc);
+                
+                assembler->buf[strlen(assembler->buf) - 1] = 0;
+                assembler_register_label(
+                    assembler,
+                    assembler->buf,
+                    assembler->pc
+                );
+            } break;
+            
             case LIST_ITEM: printf("LIST_ITEM"); break;
             case LIST_END:  printf("LIST_END"); break;
-            case SYMBOL:    printf("SYMBOL"); break;
+            
+            case SYMBOL: {
+                if (!strcmp(assembler->buf, "ref")) {
+                    printf("LABEL_REF@");
+                    
+                    read_symbol(assembler);
+                    switch(get_symbol_type(assembler)) {
+                        case SYMBOL: {
+                            int label_index = assembler_get_label(
+                                assembler, assembler->buf
+                            );
+                            
+                            if (label_index != -1) {
+                                uint64_t label_value =
+                                    assembler->ltab->labels[label_index].value
+                                    & ((1UL << 63) - 1);
+                                assembler->work_area[assembler->pc] =
+                                    label_value;
+                                printf("%lu", label_value);
+                            }
+                            
+                            else {
+                                int thunk = assembler_insert_thunk(
+                                    assembler,
+                                    assembler->buf,
+                                    assembler->pc,
+                                    0,
+                                    63,
+                                    0
+                                );
+                                printf(
+                                    "%s",
+                                    thunk == -1 ? "ERROR" : "THUNK"
+                                );
+                            }
+                        } break;
+                        
+                        default: printf("ERROR");
+                    }
+                } else {
+                    printf("SYMBOL");
+                }
+                assembler->pc++;
+            } break;
         }
+        
         printf("\n");
     }
     
@@ -219,6 +325,12 @@ int main(int argc, char *argv[]) {
         : assembler->error ? "error"
         :                    "impossible"
     );
+    
+    if (assembler->ttab->num_thunks == 0) {
+        printf("All references resolved\n");
+    } else {
+        printf("%d unresolved references\n", assembler->ttab->num_thunks);
+    }
     
     delete_assembler(assembler);
     return 0;
