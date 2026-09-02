f2e774ebf235cf4039db6cb749de6a7542d80c7e
vmlinuz719 <vmlinuz719@gmail.com>
2026-09-02 15:14:48 -0400

    nah I don't like that

--- ch_7310.c
+++ ch_7310.c
@@ -20,7 +20,7 @@ void ch7310_detach(acr7k_subch_t *subch) {
     ch7310_device_t *device = subch->device;
     
     pthread_cancel(subch->thread);
-    fprintf(stderr, "MSC: 7310/%04o:%02o detached\n",
+    fprintf(stderr, "7310: %04o:%02o detached\n",
         device->ch_id, device->sch_id);
     free(subch->device);
 }
@@ -38,7 +38,7 @@ void ch7310_sense(
     uint64_t addr
 ) {
     ch7310_device_t *device = subch->device;
-    fprintf(stderr, "MSC: 7310/%04o:%02o SENSE(%02o, %09o)\n",
+    fprintf(stderr, "7310: %04o:%02o SENSE(%02o, %09o)\n",
         device->ch_id, device->sch_id,
         (unsigned int) opcode, (unsigned int) addr);
 }
@@ -50,7 +50,7 @@ void ch7310_control(
     uint64_t addr
 ) {
     ch7310_device_t *device = subch->device;
-    fprintf(stderr, "MSC: 7310/%04o:%02o CONTROL(%02o, %09o)\n",
+    fprintf(stderr, "7310: %04o:%02o CONTROL(%02o, %09o)\n",
         device->ch_id, device->sch_id,
         (unsigned int) opcode, (unsigned int) addr);
 }
@@ -63,7 +63,7 @@ void ch7310_start_transact(
 ) {
     ch7310_device_t *device = subch->device;
     
-    fprintf(stderr, "MSC: 7310/%04o:%02o Start Transaction %s(%02o)\n",
+    fprintf(stderr, "7310: %04o:%02o Start Transaction %s(%02o)\n",
         device->ch_id, device->sch_id,
         write ? "WRITE" : "READ", (unsigned int) opcode);
     
@@ -78,7 +78,7 @@ void ch7310_end_transact(
 ) {
     ch7310_device_t *device = subch->device;
     
-    fprintf(stderr, "MSC: 7310/%04o:%02o End Transaction %s(%02o)\n",
+    fprintf(stderr, "7310: %04o:%02o End Transaction %s(%02o)\n",
         device->ch_id, device->sch_id,
         device->write ? "WRITE" : "READ", (unsigned int) device->tx_type);
     
@@ -96,7 +96,7 @@ void ch7310_transfer(
 ) {
     ch7310_device_t *device = subch->device;
     
-    fprintf(stderr, "MSC: 7310/%04o:%02o     %s(%02o) %09o[%03o]\n",
+    fprintf(stderr, "7310: %04o:%02o     %s(%02o) %09o[%03o]\n",
         device->ch_id, device->sch_id,
         device->write ? "WRITE" : "READ", (unsigned int) device->tx_type,
         (unsigned int) tx_addr, (unsigned int) count);
@@ -106,12 +106,12 @@ void init_7310(acr7k_cu_t *cpu, int id, int sc_id) {
     acr7k_msch_t *ctx = (acr7k_msch_t *) cpu->ioctx[id];// Unsafe horrible hack
     
     if (sc_id < 0 || sc_id > 15) {
-        fprintf(stderr, "MSC: 7310/%04o invalid subchannel %02o\n", id, sc_id);
+        fprintf(stderr, "7310: %04o invalid subchannel %02o\n", id, sc_id);
         return;
     }
     
     else if (ctx->subchannel[sc_id].attached) {
-        fprintf(stderr, "MSC: 7310/%04o:%02o already attached\n", id, sc_id);
+        fprintf(stderr, "7310: %04o:%02o already attached\n", id, sc_id);
         return;
     }
     
@@ -130,7 +130,7 @@ void init_7310(acr7k_cu_t *cpu, int id, int sc_id) {
     subch->end_transact = ch7310_end_transact;
     subch->transfer = ch7310_transfer;
     
-    fprintf(stderr, "MSC: 7310/%04o:%02o (no configurable options)\n",
+    fprintf(stderr, "7310: %04o:%02o (no configurable options)\n",
         device->ch_id, device->sch_id);
         
     sc_attach(cpu, id, sc_id);
